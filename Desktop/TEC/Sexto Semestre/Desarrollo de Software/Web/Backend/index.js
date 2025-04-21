6054a18d680786a92a09a95274c37690c05ae46f
RodrigoGzz <117121154+RodrigoGzz@users.noreply.github.com>
2025-04-21 15:31:51 -0600

    Delete Desktop/TEC/Sexto Semestre/Desarrollo de Software/Web directory

--- Desktop/TEC/Sexto Semestre/Desarrollo de Software/Web/Backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-
-const express = request;
-
-app.get("/",(req, res)=> res.send("Hola mundo"))
-
-app.listen(5000, console.log("http://localhost:5000"))
-console.log("Hola")
